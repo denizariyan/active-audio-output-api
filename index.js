a1818f186d2a94fa4586f7fd328afb87636652f9
Deniz Ariyan <denizariyan@gmail.com>
2020-10-04 01:31:03 +0300

    fix require being let

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let express = require('express');
-let app = express();
-let exec = require("child_process").exec;
-let path = require('path');
+const express = require('express');
+const app = express();
+const exec = require("child_process").exec;
+const path = require('path');
 
 const relPath = path.join(__dirname, 'external', 'EndPointController.exe');
 
