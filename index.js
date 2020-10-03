8d92f9d746d49b0075fb1ff8226d5ec431a095d3
Deniz Ariyan <denizariyan@gmail.com>
2020-10-04 00:08:06 +0300

    Added external code and set the API endpoints

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
+let exec = require("child_process").exec;
+let path = require('path');
 
-app.get('/', function (req, res) {
+const relPath = path.join(__dirname, 'external', 'EndPointController.exe');
 
-})
+app.post('/out', function (req, res) {
+    exec(relPath + " 6");
+    res.sendStatus(200)
+});
 
-var server = app.listen(8081)
+app.post('/in', function (req, res) {
+    exec(relPath + " 3");
+    res.sendStatus(200)
+});
+
+const PORT = 8081;
+let server = app.listen(PORT)
+console.log(`Server running on ${PORT}`);
