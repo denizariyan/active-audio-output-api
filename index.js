b0a8f686b3501a62a0e3e5a4b74e650e6162e90e
Deniz Ariyan <denizariyan@gmail.com>
2020-10-04 19:10:24 +0300

    Algorithm explanation

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require("express");
 const app = express();
 const exec = require("child_process").exec;
 const path = require("path");
-
 const relPath = path.join(__dirname, "external", "EndPointController.exe");
+
+/**
+ * Have to use a var like this because ESP8266 is just blasting the state of
+ * the button and if I try to set the output all the time I cant listen to anything
+ * since it cuts the the audio for a split second every time the audio output is set
+ *
+ * @var {string} lastReq
+ */
 let lastReq = "";
 
 app.post("/out", function (req, res) {
