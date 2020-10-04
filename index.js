adf1c4ed256911dad928ef9f8dc073ac4bbff7f3
Deniz Ariyan <denizariyan@gmail.com>
2020-10-04 16:13:21 +0300

    Change algorithm behavior to meet requirements for lacking hardware
    Changed how the endpoint behaves so I can use a switch instead of a button as I couldn't get a button delivered in a timely manner. Thanks COVID!

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const exec = require("child_process").exec;
-const path = require('path');
+const path = require("path");
 
-const relPath = path.join(__dirname, 'external', 'EndPointController.exe');
+const relPath = path.join(__dirname, "external", "EndPointController.exe");
+let lastReq = "";
 
-app.post('/out', function (req, res) {
+app.post("/out", function (req, res) {
+  if (lastReq !== "out") {
     exec(relPath + " 6"); //6 for outer speakers
-    res.sendStatus(200)
+  }
+  res.sendStatus(200);
+  lastReq = "out";
 });
 
-app.post('/in', function (req, res) {
+app.post("/in", function (req, res) {
+  if (lastReq !== "in") {
     exec(relPath + " 3"); //3 for headsets
-    res.sendStatus(200)
+  }
+  res.sendStatus(200);
+  lastReq = "in";
 });
 
 const PORT = 8081;
-let server = app.listen(PORT)
+let server = app.listen(PORT);
 console.log(`Server running on ${PORT}`);
