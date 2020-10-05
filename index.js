2e918742e3de191a208b6c05a7d9e8c9f2d0a17c
Deniz Ariyan <denizariyan@gmail.com>
2020-10-05 03:22:59 +0300

    Added ESLint config and clarified source

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const exec = require("child_process").exec;
-const path = require("path");
-const relPath = path.join(__dirname, "external", "EndPointController.exe");
+const exec = require('child_process').exec;
+const path = require('path');
+const relPath = path.join(__dirname, 'external', 'EndPointController.exe');
 
 /**
  * Have to use a var like this because ESP8266 is just blasting the state of
- * the button and if I try to set the output all the time I cant listen to anything
- * since it cuts the the audio for a split second every time the audio output is set
+ * the button and if I try to set the output all the time I cant listen to
+ * anything since it cuts the the audio for a split second every time
+ * the audio output is set
  *
  * @var {string} lastReq
  */
-let lastReq = "";
+let lastReq = '';
 
-app.post("/out", function (req, res) {
-  if (lastReq !== "out") {
-    exec(relPath + " 6"); //6 for outer speakers
+app.post('/out', function(req, res) {
+  if (lastReq !== 'out') {
+    exec(relPath + ' 6'); // 6 for outer speakers
   }
   res.sendStatus(200);
-  lastReq = "out";
+  lastReq = 'out';
 });
 
-app.post("/in", function (req, res) {
-  if (lastReq !== "in") {
-    exec(relPath + " 3"); //3 for headsets
+app.post('/in', function(req, res) {
+  if (lastReq !== 'in') {
+    exec(relPath + ' 3'); // 3 for headsets
   }
   res.sendStatus(200);
-  lastReq = "in";
+  lastReq = 'in';
 });
 
 const PORT = 8081;
-let server = app.listen(PORT);
+app.listen(PORT);
 console.log(`Server running on ${PORT}`);
